b057faf0a33749c25b27ae0215119c94e4f067ec
giorgio <giorgio@calib.ro>
2019-03-21 18:37:03 +0100

    adding sidemenu and other details

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	   console.log("ciao")
+    $('#open-menu').on('click', function () {
+        $('#sidemenu').toggleClass('open');
+        console.log("ciao")
+    });
+    $('#close-menu').on('click', function () {
+        $('#sidemenu').toggleClass('open');
+        console.log("ciao")
+    });
+
+});
